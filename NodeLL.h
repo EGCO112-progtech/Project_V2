dcba2fae094b5d701acd93bfa8b500fe22153e4a
SUPAKORN-THV <supakorn.thv@student.mahidol.edu>
2023-05-15 11:47:29 +0700

    Version 8.3

--- NodeLL.h
+++ NodeLL.h
@@ -47,7 +47,7 @@ class general:public NODE{
     public:
         general(string = "No Name", string = "No MFG", string = "No EXP", long = 0);
         virtual ~general() = 0;
-        virtual void show_data() = 0;;
+        virtual void show_data() = 0;
 };
 
 
